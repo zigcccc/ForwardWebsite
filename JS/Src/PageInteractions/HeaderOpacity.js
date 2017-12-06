dbf57e81e901e9628f5936fd00440da15b242010
Žiga Krašovec <zkrasovec@gmail.com>
2017-12-06 17:43:02 +0100

    Fixed overscroll issue on header opacity

--- JS/Src/PageInteractions/HeaderOpacity.js
+++ JS/Src/PageInteractions/HeaderOpacity.js
@@ -1,16 +1,18 @@
 const headerOpacity = function (header) {
   window.addEventListener('scroll', (e) => {
     let offset = window.pageYOffset
-    console.log(offset)
+
+    // Attach class 'small' if user has scrolled the page
     if (offset > 0) {
       header.classList.add('small')
     } else {
       header.classList.remove('small')
     }
+
+    // Increase the header opacity and stop at 0.75
     if (offset <= 75) {
       header.style.backgroundColor = 'rgba(22,22,22,0)'.replace(/[^,]+(?=\))/, offset / 100)
-    }
-    else {
+    } else {
       header.style.backgroundColor = 'rgba(22,22,22,0)'.replace(/[^,]+(?=\))/, '0.75')
     }
   })
