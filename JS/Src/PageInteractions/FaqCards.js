3b04f483db5d4e4dc854827733407ddf316b031c
Žiga Krašovec <zkrasovec@gmail.com>
2017-12-15 13:46:26 +0100

    Updated accordions interactions

---
+++ JS/Src/PageInteractions/FaqCards.js
@@ -0,0 +1,21 @@
+const faqCards = (cards) => {
+  for (let card of cards) {
+    card.addEventListener('click', (e) => {
+      let cond = card.classList.contains('open')
+      let siblings = card.parentNode.children
+      if (cond) {
+        card.classList.remove('open')
+      }
+      else {
+        for (let sibling of siblings) {
+          if (sibling.classList.contains('open')) {
+            sibling.classList.remove('open')
+          }
+        }
+        card.classList.add('open')
+      }
+    })
+  }
+}
+
+export default faqCards
